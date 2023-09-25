afcbea1f4b20bb0683086226546a38c79f1b72d8
Sudeera <hashansudeera380@gmail.com>
2023-09-25 06:48:16 +0530

    continue

--- student_login.php
+++ student_login.php
@@ -6,10 +6,30 @@
     <div class = "main_title">
             <h1>Skill-<font style="color :white;">Sharp</font> Academy</h1>
         </div>
+
         <div class = "form_space">
-            
+            <h2 class="form_name">Student Login</h2>
             <form>
-                <input type = "text" name = "Firstname"> 
+                <table>
+                    <tr>
+                        <td><label>User Name</label></td>
+                    </tr>
+                    <tr>
+                        <td><input type = "text" name = "username" class="input"> </td>
+                    </tr>
+                    <tr>
+                        <td><label>Password</label></td>
+                    </tr>
+                    <tr>
+                        <td><input type = "password" name = "username" class="input"> </td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="submit"></td>
+                    </tr>
+                    
+                </table>
+                
+                
             </form>
         </div>
 
