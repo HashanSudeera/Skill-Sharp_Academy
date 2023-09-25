29bedc7d4ad5cafc65f7b22b477a9ed4c4425bcc
Sudeera <hashansudeera380@gmail.com>
2023-09-26 00:44:00 +0530

    making ragistration form

---
+++ student_singup.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/favicon.png">
+        <title>Student Registration</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body class="teacher_singup_page">
+    <div class = "main_title">
+            <h1>Skill-<font style="color :white;">Sharp</font> Academy</h1>
+        </div>
+    
+        <div class = "singup_form_space">
+            <h2 style="color: white; font-size:35px;">Student Registration</h2>
+            <form>
+                <div class = "table_align">
+                <table>
+                    <tr>
+                        <td class="firstcolum"><label>FirstName</label></td>
+                        <td class="firstcolum1"><label>LastName</label></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="firstname" class="input1"></td>
+                        <td class="firstcolum1"><input type="text" name="lastname" class="input1"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Birth date</label></td>
+                        <td class="firstcolum1"><label>City</label></td>
+                       
+                    </tr>
+                    <tr>
+                    <td><input type="date" name = "dob" class="date_of"></td>
+                    <td class="firstcolum1"><input type="text" name="city" class="input1"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Email</label></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="text" name="email" class="inputemail"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Password</label></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="password" name="password" class="inputemail"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Retype Password</label></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="password" name="confirm_password" class="inputemail"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="r_button"><input type="submit" value="Register" class="register_button"></td>
+                    </tr>
+                    
+                </table>
+                </div>
+            </form>
+        </div>
+    
+    </body>
+</html>
