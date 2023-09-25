ba4d19b24d8ab6c7fe0ca083b6bdb3bc8ad26ad0
Sudeera <hashansudeera380@gmail.com>
2023-09-25 05:57:32 +0530

    create style folder

---
+++ student_login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class = "form_space">
+            <h1>Skill-Sharp Academy Login</h1>
+            <form>
+                <input type = "text" name = "Firstname"> 
+            </form>
+        </div>
+
+    </body>
+</html>
