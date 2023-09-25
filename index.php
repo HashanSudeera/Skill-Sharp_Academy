9f7808bbf64c8f2433599e5be8c201b578da6d69
Sudeera <hashansudeera380@gmail.com>
2023-09-25 05:37:00 +0530

    add all files

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class = "main_title">
+            <h1>Skill-<font style="color :white;">Sharp</font> Academy</h1>
+        </div>
+
+        <div class="student">
+            <img src="img/student.png" alt="image not found" class="img">
+            <div class="text-block">
+                <a href="student_login.php">Student Login</a>
+            </div>
+        </div>
+        <div class="teacher">
+            <img src="img/teacher.jpg" alt="image not found" class="img">
+            <div class="text-block1">
+                <a href="teacher_login.php">Teacher Login</a>
+            </div>
+        </div>
+
+    </body>
+</html>
